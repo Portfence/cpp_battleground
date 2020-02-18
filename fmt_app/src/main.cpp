9ecedea6cf4db6c3ef4ffdd0604f3bba8855c449
Albert Havnegjerde <albert.havnegjerde@kdef.cloud>
2020-02-18 14:25:18 +0100

    Add Format App using Fmt library

---
+++ fmt_app/src/main.cpp
@@ -0,0 +1,20 @@
+/**
+ * @file main.cpp
+ * @brief Testing fmt library with C++20
+ * @version 0.1
+ * @date 2020-02-18
+ *
+ * @copyright Copyright (c) 2020
+ *
+ */
+#include <iostream>
+#include <fmt/format.h>
+
+int main()
+{
+  std::string s = fmt::format(FMT_STRING("{:d}"), 42); // check string format index at compile time
+  std::string s2 = fmt::format(FMT_STRING("this string {}, is cool {}"), 42, 43); // check string format index at compile time
+  std::cout << s << std::endl;
+  std::cout << s2 << std::endl;
+  return 0;
+}
